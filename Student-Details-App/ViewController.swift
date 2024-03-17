1862806fc67820b8a30087349c8b81fa96f4fd4a
bathalasupraja <bathalasupraja2@gmail.com>
2024-03-17 20:43:50 +0530

    Initial Commit

---
+++ Student-Details-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Student-Details-App
+//
+//  Created by Supraja on 17/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
